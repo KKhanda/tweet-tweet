af3bdef781f0eaf1354afe3a14be5f0ee912c296
qoops <ilya.v.gladyshev@gmail.com>
2018-02-23 23:48:43 +0300

    changed mutable list to var List

--- tweet-tweet-web-app/src/main/scala/com/scalacourse/models/User.scala
+++ tweet-tweet-web-app/src/main/scala/com/scalacourse/models/User.scala
@@ -2,8 +2,7 @@ package com.scalacourse.models
 
 import scala.collection.mutable
 
-case class User(id: Long, email: String, nickname: String, password: String,
-                tweets: mutable.MutableList[Twit] = mutable.MutableList.empty) {
+case class User(id: Long, email: String, nickname: String, password: String, var tweets: List[Twit] = Nil) {
 
   override def hashCode: Int = id.hashCode()
 }
